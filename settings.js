73ea7eb013b16061f061600190565013fe5667f0
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2023-04-14 16:03:28 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,9 +17,9 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
+global.BOT_TOKEN = "5894353360:AAEGCIOXSkB847Vl7XaOFd3wMquDAlY-hmE" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "ikytelegrambot" //your bot name
+global.OWNER_NAME = "ikybot" //your name
 global.OWNER_NUMBER = "62887435047326" //your telegram number
 global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
